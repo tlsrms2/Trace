f4fec49557476e5944379f36e717b4402652b5aa
tlsrms2 <tiffha090@gmail.com>
2026-03-09 20:18:33 +0900

    GameManager.cs 뼈대 추가

---
+++ Assets/External/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    public void InitGame()
+    {
+
+    }
+
+    public void PauseGame()
+    {
+
+    }
+
+    public void ContinueGame()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+
+    }
+
+    public void StopGame()
+    {
+
+    }
+}
