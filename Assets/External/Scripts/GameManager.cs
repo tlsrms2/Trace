2ca9263a6987eda425c7da4576a85fb4e6bb16ec
tlsrms2 <tiffha090@gmail.com>
2026-03-10 09:12:13 +0900

    GameManager 수정

--- Assets/External/Scripts/GameManager.cs
+++ Assets/External/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
+public enum GamePhase { Paused, Replay, RealTime, GameOver }
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public bool IsPaused { get; private set; }
-
+    public GamePhase CurrentPhase = GamePhase.RealTime;
+    
     [Header("Gauge Settings")]
     [SerializeField] private float MaxGauge = 100f;
     [SerializeField] private float CurrentGauge;
@@ -18,10 +20,25 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+        if (CurrentPhase == GamePhase.GameOver) return;
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             TogglePause();
         }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            switch (CurrentPhase)
+            {
+                case GamePhase.Paused:
+                    ChangePhase(GamePhase.Replay); 
+                    break;
+                case GamePhase.RealTime:
+                    ChangePhase(GamePhase.Paused);
+                    break;
+            }
+        }
+        HandleGauge();
     }
 
     public void TogglePause()
@@ -46,21 +63,59 @@ public class GameManager : MonoBehaviour
         UIManager.Instance.HidePause();
     }
 
-    public void ConsumeGauge()
+    public float GetCurrentGauge()
     {
-        CurrentGauge -= ConsumptionRate * Time.deltaTime;
-        if (CurrentGauge < 0f)
+        return CurrentGauge;
+    }
+    
+    public float GetGaugePercentage()
+    {
+        return CurrentGauge / MaxGauge;
+    }
+
+    private void HandleGauge()
+    {
+        if (CurrentPhase == GamePhase.Paused)
         {
-            CurrentGauge = 0f;
+            CurrentGauge -= ConsumptionRate * Time.unscaledDeltaTime;
+            if (CurrentGauge <= 0)
+            {
+                CurrentGauge = 0;
+                ChangePhase(GamePhase.Replay); 
+            }
+        }
+        else if (CurrentPhase == GamePhase.RealTime)
+        {
+            CurrentGauge = Mathf.Min(MaxGauge, CurrentGauge + RecoveryRate * Time.deltaTime);
         }
     }
 
-    public void RecoverGauge()
+    public void ChangePhase(GamePhase nextPhase)
     {
-        CurrentGauge += RecoveryRate * Time.deltaTime;
-        if (CurrentGauge > MaxGauge)
+        if (CurrentPhase == nextPhase) return;
+
+        CurrentPhase = nextPhase;
+
+        switch (nextPhase)
         {
-            CurrentGauge = MaxGauge;
+            case GamePhase.Paused:
+                Time.timeScale = 0f; 
+                UIManager.Instance.ShowPause();
+                break;
+
+            case GamePhase.Replay:
+                Time.timeScale = 1f;
+                UIManager.Instance.HidePause();
+                break;
+
+            case GamePhase.RealTime:
+                Time.timeScale = 1f;
+                UIManager.Instance.HidePause();
+                break;
+
+            case GamePhase.GameOver:
+                Time.timeScale = 0f;
+                break;
         }
     }
 }
