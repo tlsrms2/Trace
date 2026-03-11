f44224208a2c9d17749b95d6e7c9464297223f47
emotionalfrontier <emotionalfrontier@gmail.com>
2026-03-11 14:56:34 +0900

    Revert "Merge branch 'main' of https://github.com/tlsrms2/Trace"
    This reverts commit c6f4c81ab32be4c59627b0d5c57787fc97948650, reversing
    changes made to 32ace2b80e5e6387565c4a0f1cedc46b07e10255.

--- Assets/External/Scripts/Manager/AudioManager.cs
+++ Assets/External/Scripts/Manager/AudioManager.cs
@@ -4,23 +4,10 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
 
-    [Header("Audio Sources")]
     [SerializeField] private AudioSource bgmSource;
     [SerializeField] private AudioSource sfxSource;
 
-    [Header("BGM Clips")]
-    [SerializeField] private AudioClip titleBgm;
-    [SerializeField] private AudioClip ingameBgm;
-
-    [Header("SFX Clips")]
-    [SerializeField] private AudioClip buttonClickSfx;
-    [SerializeField] private AudioClip laserPlaceSfx;
-    [SerializeField] private AudioClip laserChargeSfx;
-    [SerializeField] private AudioClip playerDeathSfx;
-    [SerializeField] private AudioClip enemyDeathSfx;
-    [SerializeField] private AudioClip bossAppearSfx;
-
-    private void Awake()
+    void Awake()
     {
         if (Instance != null && Instance != this)
         {
@@ -32,30 +19,15 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    #region BGM
     public void PlayBgm(AudioClip clip)
     {
-        if (bgmSource.clip == clip) return; // 같은 BGM이면 재생하지 않음
         bgmSource.clip = clip;
         bgmSource.loop = true;
         bgmSource.Play();
     }
 
-    public void PlayTitleBgm() => PlayBgm(titleBgm);
-    public void PlayIngameBgm() => PlayBgm(ingameBgm);
-    #endregion
-
-    #region SFX
     public void PlaySfx(AudioClip clip)
     {
         sfxSource.PlayOneShot(clip);
     }
-
-    public void PlayButtonClick() => PlaySfx(buttonClickSfx);
-    public void PlayLaserPlace() => PlaySfx(laserPlaceSfx);
-    public void PlayLaserCharge() => PlaySfx(laserChargeSfx);
-    public void PlayPlayerDeath() => PlaySfx(playerDeathSfx);
-    public void PlayEnemyDeath() => PlaySfx(enemyDeathSfx);
-    public void PlayBossAppear() => PlaySfx(bossAppearSfx);
-    #endregion
 }
