c087b583c99a87ab5d72771f361d7b894c05ac24
WhendyInJ <whendy3680@gmail.com>
2026-03-11 14:24:58 +0900

    오디오 매니저 업데이트

--- Assets/External/Scripts/Manager/AudioManager.cs
+++ Assets/External/Scripts/Manager/AudioManager.cs
@@ -4,10 +4,23 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
 
+    [Header("Audio Sources")]
     [SerializeField] private AudioSource bgmSource;
     [SerializeField] private AudioSource sfxSource;
 
-    void Awake()
+    [Header("BGM Clips")]
+    [SerializeField] private AudioClip titleBgm;
+    [SerializeField] private AudioClip ingameBgm;
+
+    [Header("SFX Clips")]
+    [SerializeField] private AudioClip buttonClickSfx;
+    [SerializeField] private AudioClip laserPlaceSfx;
+    [SerializeField] private AudioClip laserChargeSfx;
+    [SerializeField] private AudioClip playerDeathSfx;
+    [SerializeField] private AudioClip enemyDeathSfx;
+    [SerializeField] private AudioClip bossAppearSfx;
+
+    private void Awake()
     {
         if (Instance != null && Instance != this)
         {
@@ -19,15 +32,30 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    #region BGM
     public void PlayBgm(AudioClip clip)
     {
+        if (bgmSource.clip == clip) return; // 같은 BGM이면 재생하지 않음
         bgmSource.clip = clip;
         bgmSource.loop = true;
         bgmSource.Play();
     }
 
+    public void PlayTitleBgm() => PlayBgm(titleBgm);
+    public void PlayIngameBgm() => PlayBgm(ingameBgm);
+    #endregion
+
+    #region SFX
     public void PlaySfx(AudioClip clip)
     {
         sfxSource.PlayOneShot(clip);
     }
+
+    public void PlayButtonClick() => PlaySfx(buttonClickSfx);
+    public void PlayLaserPlace() => PlaySfx(laserPlaceSfx);
+    public void PlayLaserCharge() => PlaySfx(laserChargeSfx);
+    public void PlayPlayerDeath() => PlaySfx(playerDeathSfx);
+    public void PlayEnemyDeath() => PlaySfx(enemyDeathSfx);
+    public void PlayBossAppear() => PlaySfx(bossAppearSfx);
+    #endregion
 }
